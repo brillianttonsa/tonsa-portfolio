ca6f970ca37989eb1ff5fadc3f7b0c48471b96fc
brillianttonsa <abdullatifmnyamis@gmail.com>
2026-01-03 00:54:54 +0300

    footer component add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-slate-950 p-6 text-slate-500 text-sm text-center">
+        <p>© {new Date().getFullYear()} Developer Portfolio • React & Tailwindcss</p>
+        <p className="font-semibold">Built by Abdullatif Heri</p>
+
+
+
+    </footer>
+  );
+};
+
+export default Footer;
